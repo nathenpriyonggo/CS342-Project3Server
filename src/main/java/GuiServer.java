98f9814ecc08bbd6344d2d59b9186c12154c6a3b
Nathen Priyonggo <nagapri16@gmail.com>
2024-04-03 16:56:27 -0500

    started

--- src/main/java/GuiServer.java
+++ src/main/java/GuiServer.java
@@ -1,20 +1,25 @@
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class GuiServer extends Application{
 
@@ -30,9 +35,11 @@ public class GuiServer extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+
+
 		serverConnection = new Server(data -> {
 			Platform.runLater(()->{
-				listItems.getItems().add(data.toString());
+				listItems.getItems().add(0, data.toString());
 			});
 		});
 
@@ -58,16 +65,23 @@ public class GuiServer extends Application{
 	}
 	
 	public Scene createServerGui() {
-		
-		BorderPane pane = new BorderPane();
+
+		Font.loadFont(getClass().getResourceAsStream("gg-sans-2/gg sans Regular.ttf"), 14);
+		Font.loadFont(getClass().getResourceAsStream("gg-sans-2/gg sans Medium.ttf"), 14);
+		Font.loadFont(getClass().getResourceAsStream("gg-sans-2/gg sans Semibold.ttf"), 14);
+		Font.loadFont(getClass().getResourceAsStream("gg-sans-2/gg sans Bold.ttf"), 14);
+
+        BorderPane pane = new BorderPane();
 		pane.setPadding(new Insets(70));
-		pane.setStyle("-fx-background-color: coral");
-		
-		pane.setCenter(listItems);
-		pane.setStyle("-fx-font-family: 'serif'");
-		return new Scene(pane, 500, 400);
-		
-		
+		pane.setStyle("-fx-background-color: coral;" + "-fx-font-family: 'gg sans Bold'");
+
+		ToggleButton toggleButton = new ToggleButton("Toggle me");
+
+
+		VBox paneVertical = new VBox(10, toggleButton, listItems);
+        pane.setCenter(paneVertical);
+
+		return new Scene(pane, 600, 600);
 	}
 
 
